05b84f31d799ce12d2f9144d816872bef78f10da
belikovanastasya <belikova.ava@gmail.com>
2019-03-19 22:03:08 +0200

    create back api

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Require routes They are yet to be created
 var index = require('./routes/index');
@@ -12,7 +14,28 @@ const api = require('./routes/api/index');
 // Setup an express app
 var app = express();
 // ************************** Database connection here ********************
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`, {
+  autoIndex: false,
+  useNewUrlParser: true
+});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // ************************** CORS config will be here ******************** view
+app.all('/*', function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res
+      .status(200)
+      .end();
+  } else {
+    next();
+  }
+});
 // engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,7 +46,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
-app.use('/users', users);
+//app.use('/users', users);
 app.use('/api/v1', api)
+const port = 3001;
 
+app.listen(port, () => {
+  console.log(`Server is up and running on port number ${port}`);
+});
 module.exports = app;
